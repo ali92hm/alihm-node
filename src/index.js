31b6c4bc08e173ec317f9ba49f4972dd4aecb881
ali92hm <ali@alihm.net>
2019-11-19 16:38:45 -0800

    added my card

---
+++ src/index.js
@@ -0,0 +1,39 @@
+const chalk = require('chalk')
+const boxen = require('boxen')
+
+// Define options for Boxen
+const options = {
+  padding: 1,
+  margin: 1,
+  borderStyle: 'round'
+}
+
+// Text + chalk definitions
+const data = {
+  name: chalk.white('Ali Hajimirza /'),
+  handle: chalk.cyan('ali92hm'),
+  work: chalk.white('Software Engineer @ Convoy'),
+  twitter: chalk.cyan('https://twitter.com/ali92hm'),
+  github: chalk.cyan('https://github.com/ali92hm'),
+  linkedin: chalk.cyan('https://www.linkedin.com/in/ali92hm'),
+  web: chalk.cyan('https://alihm.net'),
+  labelWork: chalk.white.bold('      Work:'),
+  labelTwitter: chalk.white.bold('   Twitter:'),
+  labelGitHub: chalk.white.bold('    GitHub:'),
+  labelLinkedIn: chalk.white.bold('  LinkedIn:'),
+  labelWeb: chalk.white.bold('       Web:'),
+}
+
+// Actual strings we're going to output
+const newline = '\n'
+const heading = `${data.name} ${data.handle}`
+const working = `${data.labelWork}  ${data.work}`
+const twittering = `${data.labelTwitter}  ${data.twitter}`
+const githubing = `${data.labelGitHub}  ${data.github}`
+const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`
+const webing = `${data.labelWeb}  ${data.web}`
+
+// Put all our output together into a single variable so we can use boxen effectively
+const output = heading + newline + newline + working + newline + twittering + newline + githubing + newline + linkedining + newline + webing + newline + newline
+
+console.log(chalk.green(boxen(output, options)))
